3a9612325467ee0cf3adfde94de48e6c6b402e82
sakkees <sakkees@users.noreply.github.com>
2020-03-10 13:54:13 +0100

    Removed unused imports

--- src/main/java/com/example/prioritizationtool/PrioritizationToolApplication.java
+++ src/main/java/com/example/prioritizationtool/PrioritizationToolApplication.java
@@ -1,11 +1,7 @@
 package com.example.prioritizationtool;
 
-import com.example.prioritizationtool.controller.Controller;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PrioritizationToolApplication {
