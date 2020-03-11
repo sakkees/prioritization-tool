9f93625d706cb88a3f5d7a9002d3834a990a2b2a
sakkees <sakkees@users.noreply.github.com>
2020-03-11 14:57:28 +0100

    new method style, all database code moved to repository

--- src/main/java/com/example/prioritizationtool/controller/Controller.java
+++ src/main/java/com/example/prioritizationtool/controller/Controller.java
@@ -4,8 +4,12 @@ import com.example.prioritizationtool.model.Issue;
 import com.example.prioritizationtool.service.IssueService;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,110 +24,51 @@ public class Controller {
                     .setProjectId(projectId)
                     .build();
     Firestore db = firestoreOptions.getService();
-
+    CollectionReference collectionRef = db.collection("issues");
     DocumentReference docRef;
-    /*
+
+
     @Autowired
-    IssueService issueService;
+    private IssueService issueService;
 
-    @RequestMapping("/issues")
-    public ResponseEntity<Object> getAllIssues() {
-        return new ResponseEntity<Issue>(IssueService.findAll(), HttpStatus.OK);
-    }
-    @RequestMapping("/issue")
-    public ResponseEntity<Object> getIssueById() {
-        return new ResponseEntity<Issue>(IssueService.getIssue(), HttpStatus.OK);
-    }
-    @PutMapping("/update")
-    public ResponseEntity<Object> updateIssue() {
-        return new ResponseEntity<Issue>(IssueService.updateIssue(), HttpStatus.OK);
-    }
     @PostMapping("/create")
-    public ResponseEntity<Object> createIssue(){
-        return new ResponseEntity<Issue>(IssueService.createIssue(), HttpStatus.OK);
+    public Issue addIssue1(@RequestBody Issue issue){
+        issueService.addIssue(issue);
+        return issue;
     }
-    @DeleteMapping("/delete/issue")
-    public ResponseEntity<Object> deleteIssue(){
-        return new ResponseEntity<Issue>(IssueService.deleteIssue(), HttpStatus.OK);
-    }
-    */
 
-    @PostMapping(path = "/api/add")
-    public String addIssue(@RequestBody Map<String, Object> payload) {
-        String title = "";
+    @DeleteMapping("/delete")
+    public String deleteIssue(@RequestBody Map<String, Object> payload){
+        Issue issue;
         for(Map.Entry<String, Object> entry : payload.entrySet()){
-           // Issue issue = new Issue(entry.getKey().toString(), entry.getValue().toString());
             if(entry.getKey().equals("title")){
-                title = entry.getValue().toString();
+                issue = issueService.getIssue(entry.getValue().toString());
+                issueService.deleteIssue(issue);
             }
         }
-        docRef = db.collection("issues").document(title);
-        //asynchronously write data
-        ApiFuture<WriteResult> result = docRef.set(payload);
-        return title + " added in database.";
+        return "Deleted!";
     }
 
-    /*  Get and convert the document into an issue object
-    *   returns an issue object */
-    @GetMapping("/api/get")
-    public Issue getIssue(){
+    /* Uses same method as create issue */
+    @PutMapping("/update")
+    public Issue updateIssue(@RequestBody Issue issue) {
+        issueService.addIssue(issue);
+        return issue;
+    }
+
+    @RequestMapping("/issue")
+    public Issue getIssueById(@RequestBody Map<String, Object> payload) {
         Issue issue = null;
-        DocumentReference docRef = db.collection("issues").document("issue");
-        // asynchronously retrieve the document
-        ApiFuture<DocumentSnapshot> future = docRef.get();
-        // block on response
-        DocumentSnapshot document = null;
-        try {
-            document = future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-        if (document.exists()) {
-            // convert document to POJO (Plain Old Java Object)
-            issue = document.toObject(Issue.class);
-            System.out.println(issue.getTitle());
-        } else {
-            System.out.println("No such document!");
+        for(Map.Entry<String, Object> entry : payload.entrySet()){
+            if(entry.getKey().equals("title")){
+                issue = issueService.getIssue(entry.getValue().toString());
+            }
         }
         return issue;
     }
-    private IssueService issueService;
 
-    /* Get collection and returns a string of all the documents keys and values */
-    @GetMapping("/api/collection")
-    public String getCollection() {
-        StringBuilder collection = new StringBuilder();
-        // asynchronously retrieve all issues
-        ApiFuture<QuerySnapshot> query = db.collection("issues").get();
-        // query.get() blocks on response
-        QuerySnapshot querySnapshot = null;
-        try {
-            querySnapshot = query.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-        List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
-        for (QueryDocumentSnapshot document : documents) {
-           // issueService.addIssue(document.toObject(Issue.class));
-            collection.append(document.getString("title"))
-                    .append(" : ")
-                    .append(document.getString("description"))
-                    .append(", ");
-        }
-        //return issueService.findAll();
-        return collection.toString();
-    }
-    /* Test for adding something in database */
-    @PostMapping("/api/addIsac")
-    public String addData() {
-        String firstName = "Isac";
-        String lastName = "Larsson";
-        docRef = db.collection("users").document(firstName);
-        Map<String, Object> data = new HashMap<>();
-        data.put("name", firstName);
-        data.put("description", lastName);
-        //asynchronously write data
-        ApiFuture<WriteResult> result = docRef.set(data);
-        return firstName + " " + lastName + " added in database.";
+    @RequestMapping("/list")
+    public ArrayList<Issue> getAllIssues() {
+        return issueService.findAll();
     }
 }
