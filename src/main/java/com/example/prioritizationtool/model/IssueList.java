b89776b7465c3c91da610b7b65cbe5b30eb68f83
sakkees <sakkees@users.noreply.github.com>
2020-03-13 10:57:18 +0100

    Now extends generic List<E>

--- src/main/java/com/example/prioritizationtool/model/IssueList.java
+++ src/main/java/com/example/prioritizationtool/model/IssueList.java
@@ -1,38 +1,17 @@
 package com.example.prioritizationtool.model;
 
-import java.util.ArrayList;
+public class IssueList<E extends Item> extends List<E> {
 
-public class IssueList{
-    private ArrayList<Issue> issueList;
-    private String owner;
-
-    public IssueList(String owner){
-        this.issueList = new ArrayList<Issue>();
-        this.owner = owner;
+    public IssueList(String owner) {
+        super(owner);
     }
 
-    public void addIssue(Issue issue){
-        issueList.add(issue);
-    }
-
-    public void deleteIssue(Issue issue){
-        issueList.remove(issue);
-    }
-
-    public Issue getIssue(String title){
-        for(Issue issue : issueList){
-            if(issue.getTitle().equals(title)){
-                return issue;
+    public E get(String id) {
+        for (E e : super.list) {
+            if (e.getTitle().equals(id)) {
+                return e;
             }
         }
-      return null;
-    }
-
-    public String getOwner(){
-        return this.owner;
-    }
-
-    public boolean containsIssue(Issue issue){
-        return issueList.contains(issue);
+        return null;
     }
 }
