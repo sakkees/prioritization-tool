48972d3101dfb9e105566c560b3ce1504a576033
sakkees <sakkees@users.noreply.github.com>
2020-04-03 16:07:50 +0200

    Removed unnecessary imports

--- src/test/java/com/example/prioritizationtool/PrioritizationToolApplicationTests.java
+++ src/test/java/com/example/prioritizationtool/PrioritizationToolApplicationTests.java
@@ -1,10 +1,7 @@
 package com.example.prioritizationtool;
 
-import com.example.prioritizationtool.model.Issue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class PrioritizationToolApplicationTests {
